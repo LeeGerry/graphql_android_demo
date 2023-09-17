e202a718ce54943dca499a162905bd0db574308b
gerry <gerry>
2023-09-17 16:23:50 -0700

    增加use case

---
+++ app/src/main/java/com/zero/graphql/domain/GetCountryUseCase.kt
@@ -0,0 +1,11 @@
+package com.zero.graphql.domain
+
+class GetCountryUseCase(
+    private val countryClient: CountryClient,
+) {
+
+    suspend fun execute(code: String): DetailedCountry? {
+        return countryClient.getCountry(code)
+    }
+
+}
